84fbe438aad4838637b7822f0dc5b85062cf7500
Andy Dao <andydao2020@gmail.com>
2024-10-27 15:22:56 -0500

    helper txt and class diagrams

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 // SPA 4
-// Authors: Andy Dao, Helina
+// Authors: Andy Dao, Helina Mulugeta
 #include <iostream>
+#include <fstream>
 #include <string>
 using std::cout;
 using std::string;
 using std::endl;
 
 void describeGoal() {
-    cout << "Hello, World!" << std::endl;
+    // cout << "Hello, World!" << std::endl;
 }
 
+// Stackoverflow post for reading .txt files
+// https://stackoverflow.com/questions/6051865/how-to-read-and-write-to-a-text-file-in-c
 void help() {
-    describeGoal();
+    cout << "Needs implementation";
+    // TODO Implement Help Function
 }
 
-void handleUserInput(string input) {
-    switch (input) {
-        case 'h':
-            help();
-        break;
-        default:
-            cout << "Unrecognized Command" << endl;
-        break;
-    }
-}
+// void handleUserInput(string input) {
+//     switch (input) {
+//         case 'h':
+//             help();
+//         break;
+//         default:
+//             cout << "Unrecognized Command" << endl;
+//         break;
+//     }
+// }
 
 int main()
 {
-    cout << "Hello, World!" << std::endl;
+    help();
     return 0;
 }
