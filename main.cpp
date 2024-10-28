6f3c3e70a4b03d5d68ddef1f1b3629ac542cb33d
stuffby-helina <106909256+stuffby-helina@users.noreply.github.com>
2024-10-28 10:41:32 -0500

    Fix Help Text

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using std::cout;
-using std::string;
-using std::endl;
+using namespace std;
 
 void describeGoal() {
     // cout << "Hello, World!" << std::endl;
@@ -14,8 +12,15 @@ void describeGoal() {
 // Stackoverflow post for reading .txt files
 // https://stackoverflow.com/questions/6051865/how-to-read-and-write-to-a-text-file-in-c
 void help() {
-    cout << "Needs implementation";
-    // TODO Implement Help Function
+    fstream newfile;
+    newfile.open("help.txt",ios::in);
+    if (newfile.is_open()){ //checking whether the file is open
+        string tp;
+        while(getline(newfile, tp)){ //read data from file object and put it into string.
+            cout << tp << "\n"; //print the data of the string
+        }
+        newfile.close(); //close the file object.
+    }
 }
 
 // void handleUserInput(string input) {
