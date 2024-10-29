1b394835033287b80b8a040469848399a2dbfa69
stuffby-helina <106909256+stuffby-helina@users.noreply.github.com>
2024-10-29 13:37:12 -0500

    Fix CMakeLists File and Clean Input Handling
    Also added the cmake build files to the git ignore.

--- main.cpp
+++ main.cpp
@@ -1,68 +1,90 @@
 // SPA 4
 // Authors: Andy Dao, Helina Mulugeta
+
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-void describeGoal() {
-    // cout << "Hello, World!" << std::endl;
-}
-
 // Stackoverflow post for reading .txt files
 // https://stackoverflow.com/questions/6051865/how-to-read-and-write-to-a-text-file-in-c
-void help() {
+void openAndPrintFile(const string &fileName) {
     fstream newFile;
-    newFile.open("help.txt",ios::in);
+    newFile.open(fileName,ios::in);
     if (newFile.is_open()){ //checking whether the file is open
         string tp;
         while(getline(newFile, tp)){ //read data from file object and put it into string.
             cout << tp << "\n"; //print the data of the string
         }
-        newFile.close(); //close the file object.
+    }  else {
+        cout << "ERROR: " << fileName << " is missing." << endl;
+    }
+    newFile.close(); //close the file object.
+}
+
+void handleDirectionInput(const char input) {
+    switch (input) {
+        case 'n':
+            cout << "Headed North" << endl;
+            break;
+        case 's':
+            cout << "Headed South" << endl;
+            break;
+        case 'e':
+            cout << "Headed East" << endl;
+            break;
+        case 'w':
+            cout << "Headed West" << endl;
+            break;
+        default:
+            cout << "Unrecognized command. Press h for help." << endl;
+            break;
     }
 }
 
-void map() {
-    fstream newFile;
-    newFile.open("map.txt",ios::in);
-    if (newFile.is_open()){ //checking whether the file is open
-        string tp;
-        while(getline(newFile, tp)){ //read data from file object and put it into string.
-            cout << tp << "\n"; //print the data of the string
-        }
-        newFile.close(); //close the file object.
+void handleActionInput(const char input) {
+    switch (input) {
+        case 'c':
+            cout << "Tried to capture Wumpus" << endl;
+            break;
+        case 'i':
+            cout << "Viewing Inventory..." << endl;
+            break;
+        case 'l':
+            cout << "Lighting Lantern..." << endl;
+            break;
+        default:
+            handleDirectionInput(input);
+            break;
     }
 }
 
-// void handleUserInput(string input) {
-//     switch (input) {
-//         case 'h':
-//             help();
-//         break;
-//         default:
-//             cout << "Unrecognized Command" << endl;
-//         break;
-//     }
-// }
+bool handleUserInput(const char input) {
+    switch (input) {
+        case 'q':
+            return true;
+        case 'h':
+            openAndPrintFile("help.txt");
+            break;
+        case 'm':
+            openAndPrintFile("map.txt");
+            break;
+        default:
+            handleActionInput(input);
+            break;
+    }
+    return false;
+}
 
 int main()
 {
     bool quit = false;
-    help();
-    char command;
+    openAndPrintFile("help.txt");
+    string command;
     while(!quit) {
         cout << "Enter command:" << endl;
-        cin >> command;
-        if (command == 'q' || command == 'Q') {
-            quit = true;
-        } else if (command == 'h' || command == 'H') {
-            help();
-        } else if (command == 'm' || command == 'M') {
-            map();
-        } else {
-            cout << "Unrecognized command. Press h for help." << endl;
-        }
+        getline(cin, command);
+        quit = handleUserInput(tolower(command[0]));
     }
     return 0;
 }
